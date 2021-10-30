41d35c38ad8722084cd08cf45de0125253e9fa08
sravanje <50177314+sravanje@users.noreply.github.com>
2021-10-30 14:49:51 +0530

    Create test_time.py

---
+++ test_time.py
@@ -0,0 +1,13 @@
+from time import time
+
+
+def test_time(loop1,loop2):
+	st = time()
+	for i in range(loop1):
+		for j in range(loop2):
+			a = 1000**2
+	time_taken = time() - st
+	print('time taken: {} secs'.format(time_taken))
+
+
+test_time(10000,10000)
